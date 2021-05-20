79abaf64e66e26ce3a1c07917eda63f9b5f17d69
Xolmat <xolmatrw@gmail.com>
2021-05-20 03:41:15 -0700

    index page added  carousel and blog card component

--- resources/views/components/blog-card.blade.php
+++ resources/views/components/blog-card.blade.php
@@ -1,10 +1,24 @@
-<div class="card">
-    <div class="card-header">{{ __('Blog') }}</div>
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+<div class="card shadow">
+    <div class="card-body p-0">
+        <img class="card-img-top" src="https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+        <a href="#">
+            <h5 class="p-2 text-wrap font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, temporibus.</h5>
+        </a>
+        <p class="px-2 pl-2 text-wrap">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at cum deleniti dignissimos exercitationem fugiat hic, illum in iste minus nobis, pariatur praesentium quam quis reiciendis sapiente sit suscipit tenetur velit veritatis. At dignissimos est ipsa labore qui, saepe sit.
+        </p>
+        <p class="px-2 pl-2">
+            <a href="#">
+                <span class="badge badge-warning">Category</span>
+            </a>
+            <a href="#">
+                <span class="badge badge-warning">Something</span>
+            </a>
+            <a href="#">
+                <span class="badge badge-warning">Sport</span>
+            </a>
+        </p>
     </div>
 </div>
+
+
