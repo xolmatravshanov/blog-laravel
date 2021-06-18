30a336a380df1b3b8eac9bb893da1be3f2d9baec
Xolmat <xolmatrw@gmail.com>
2021-06-18 09:57:49 +0300

    18.06.2021 commited by console command

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -1,1 +1,19 @@
 <?php
+@extends('layouts.admin')
+
+
+@section('content')
+{{--    write  html here --}}
+
+
+
+@endsection
+
+
+
+@push('script')
+    <script>
+        // write script
+    </script>
+@endpush
+
